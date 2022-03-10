305752d6d33e0342e9eb9a331656bf701e24aae5
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-03-10 16:27:08 -0600

    heading render test, errors render test

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
+
+beforeEach(() => {
+    render(<ContactForm />)
+})
 
 test('renders without errors', () => {
 
 });
 
 test('renders the contact form header', () => {
-    
+    const header = screen.queryByText('Contact Form',  { exact: false })
+    expect(header).toBeInTheDocument()
+    expect(header).toBeTruthy()
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-
+    const input = screen.getByPlaceholderText('Edd')
+    fireEvent.change(input, { target: { value: 'abc' } })
+    const errors = await screen.getAllByText('Error:', { exact: false })
+    expect(errors).toHaveLength(1)
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-
+    const submitBtn = screen.getByText('Submit')
+    fireEvent.click(submitBtn)
+    const errors = await screen.getAllByText('Error:', { exact: false })
+    expect(errors).toHaveLength(3)
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
